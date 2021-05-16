e39e0c80fd00da8bab7009b2d57935830e1f8a08
Ryan Koo <rbk65@cornell.edu>
2021-05-17 00:25:34 +0900

    Apply color fixes

--- Social App/View/ApplyView.swift
+++ Social App/View/ApplyView.swift
@@ -16,76 +16,85 @@ struct ApplyView: View {
     
     var body: some View {
         
-        VStack(alignment: .leading){
-            
-            HStack{
-                Button(action: {
-                    applyData.currentView.toggle();
-                    applyData.setPostId(postId: "");
-                    applyData.setPostPosition(title: ""); applyData.setApplicationMessage(message: "")
-                }) {
-                    Image(systemName: "arrow.left.circle")
-                        .font(.title)
-                        .foregroundColor(Color("blue"))
-                }
-                
-                Text("Apply")
-                    .font(.largeTitle)
-                    .fontWeight(.heavy)
-                    .foregroundColor(.white)
-                
-                Spacer(minLength: 0)
-            }
-            .padding()
-            //.padding(.top,edges!.top)
-            // Top Shadow Effect...
-            .background(Color("bg"))
-            //.shadow(color: Color.white.opacity(0.06), radius: 5, x: 0, y: 5)
-            
-
-            Text("     Position Details:")
-                .fontWeight(.bold)
-                .foregroundColor(.white)
-            
-            Text("     \(position)")
-                .italic()
-                .foregroundColor(.white)
-            
-            Text("     @<Insert Company Name>")
-                .foregroundColor(.white)
-            
-            Divider()
-            
-            Text("      Add a short message for your application:")
-                .foregroundColor(.white)
-            
-            TextEditor(text: $applyData.applicationMessage)
-                .cornerRadius(15)
-                .background(Color(.clear))
-                .padding()
-//                .opacity(newPostData.isPosting ? 0.5 : 1)
-//                .disabled(newPostData.isPosting ? true : false)
-            
-            Button(action: {
-                applyData.applyTo(postId: postId); applyData.setPostPosition(title: ""); applyData.setPostId(postId: ""); applyData.currentView.toggle()
-            }) {
-                Text("Apply")
-                    .foregroundColor(.white)
-                    .fontWeight(.bold)
-                    .padding(.vertical)
-                    .frame(width: UIScreen.main.bounds.width - 100)
-                    .background(Color("blue"))
-                    .clipShape(Capsule())
-                    .padding()
-                    .padding(.top,10)
+        Color.white
+            .ignoresSafeArea()
+            .overlay(
+                VStack(alignment: .leading){
                     
-                Spacer(minLength: 0)
-            }
-            
-            
-        }
+                    HStack{
+                        Button(action: {
+                            applyData.currentView.toggle();
+                            applyData.setPostId(postId: "");
+                            applyData.setPostPosition(title: ""); applyData.setApplicationMessage(message: "")
+                        }) {
+                            Image(systemName: "arrow.left.circle")
+                                .font(.title)
+                                .foregroundColor(Color("blue"))
+                        }
+                        
+                        Text("Apply")
+                            .font(.largeTitle)
+                            .fontWeight(.heavy)
+                            .foregroundColor(.black)
+                        
+                        Spacer(minLength: 0)
+                    }
+                    .padding()
+                    //.padding(.top,edges!.top)
+                    // Top Shadow Effect...
+//                    .background(Color("bg"))
+                    //.shadow(color: Color.white.opacity(0.06), radius: 5, x: 0, y: 5)
+                    
+
+                    Text("     Position Details:")
+                        .fontWeight(.bold)
+                        .foregroundColor(.black)
+                    
+                    Text("     \(position)")
+                        .italic()
+                        .foregroundColor(.black)
+                    
+                    Text("     @<Insert Company Name>")
+                        .foregroundColor(.black)
+                    
+                    Divider()
+                    
+                    Text("      Add a short message for your application:")
+                        .foregroundColor(.black)
+                    
+                    TextEditor(text: $applyData.applicationMessage)
+                        .cornerRadius(15)
+                        .overlay(
+                            RoundedRectangle(cornerRadius: 15)
+                                .stroke(Color.black, lineWidth: 1)
+                        )
+                        .background(Color(.clear))
+                        .padding()
+        //                .opacity(newPostData.isPosting ? 0.5 : 1)
+        //                .disabled(newPostData.isPosting ? true : false)
+                    
+                    Button(action: {
+                        applyData.applyTo(postId: postId); applyData.setPostPosition(title: ""); applyData.setPostId(postId: ""); applyData.currentView.toggle()
+                    }) {
+                        Text("Apply")
+                            .foregroundColor(.white)
+                            .fontWeight(.bold)
+                            .padding(.vertical)
+                            .frame(width: UIScreen.main.bounds.width - 100)
+                            .background(Color("blue"))
+                            .clipShape(Capsule())
+                            .padding()
+                            .padding(.top,10)
+                            
+                        Spacer(minLength: 0)
+                    }
+                    
+                    
+            })
         
-        .background(Color("bg").ignoresSafeArea(.all, edges: .all))
+        
+        
+//        .background(Color("bg").ignoresSafeArea(.all, edges: .all))
         
     }
 }
